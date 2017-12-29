db51d5de541118c7dcf5f8525ecb2a44fe4643d4
XiaolonY <xiaolony@hotmail.com>
2017-12-29 23:37:05 +0800

    add a new paper

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,82 @@
 var gulp = require('gulp');
-var minifycss = require('gulp-minify-css');
+var minifycss = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
 var htmlclean = require('gulp-htmlclean');
-
 var imagemin = require('gulp-imagemin');
+var del = require('del');
+var runSequence = require('run-sequence');
+var Hexo = require('hexo');
 
-gulp.task('minify-css' , function () {
+gulp.task('clean', function() {
+    return del(['public/**/*']);
+});
+
+var hexo = new Hexo(process.cwd(), {});
+gulp.task('generate', function(cb) {
+    hexo.init().then(function() {
+        return hexo.call('generate', {
+            watch: false
+        });
+    }).then(function() {
+        return hexo.exit();
+    }).then(function() {
+        return cb()
+    }).catch(function(err) {
+        console.log(err);
+        hexo.exit(err);
+        return cb(err);
+    })
+})
+
+gulp.task('minify-css', function() {
     return gulp.src('./public/**/*.css')
-        .pipe(minifycss())
+        .pipe(minifycss({
+            compatibility: 'ie8'
+        }))
         .pipe(gulp.dest('./public'));
 });
 
-gulp.task('minify-html' , function () {
+gulp.task('minify-html', function() {
     return gulp.src('./public/**/*.html')
         .pipe(htmlclean())
         .pipe(htmlmin({
             removeComments: true,
-            collapseWhitespace: true,
-            collapseBooleanAttributes: true,
-            removeEmptyAttributes: true,
-            removeScriptTypeAttributes: true,
-            removeStyleLinkTypeAttributes: true,
             minifyJS: true,
             minifyCSS: true,
-            minifyURLs: true
+            minifyURLs: true,
         }))
-        .pipe(gulp.dest('./public'));
+        .pipe(gulp.dest('./public'))
 });
 
-gulp.task('minify-js', function () {
-    return gulp.src('./public/**/**/*.js')
+gulp.task('minify-js', function() {
+    return gulp.src('./public/**/*.js')
         .pipe(uglify())
         .pipe(gulp.dest('./public'));
 });
 
-gulp.task('images', function () {
-    gulp.src('./public/**/*.*')
-        .pipe(imagemin({
-            progressive: true
-        }))
+gulp.task('minify-img', function() {
+    return gulp.src('./public/images/**/*.*')
+        .pipe(imagemin())
         .pipe(gulp.dest('./public/images'))
+})
+
+gulp.task('minify-img-aggressive', function() {
+    return gulp.src('./public/images/**/*.*')
+        .pipe(imagemin(
+            [imagemin.gifsicle({'optimizationLevel': 3}),
+                imagemin.jpegtran({'progressive': true}),
+                imagemin.optipng({'optimizationLevel': 7}),
+                imagemin.svgo()],
+            {'verbose': true}))
+        .pipe(gulp.dest('./public/images'))
+})
+
+gulp.task('compress', function(cb) {
+    runSequence(['minify-html', 'minify-css', 'minify-js', 'minify-img-aggressive'], cb);
 });
 
-gulp.task('build', [
-    'minify-html', 'minify-css', 'minify-js', 'images'
-]);
+gulp.task('build', function(cb) {
+    runSequence('clean', 'generate', 'compress', cb)
+});
+gulp.task('default', ['build'])
