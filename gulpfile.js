3769c9d6420e4aa09d56cea387cafc6bf5cdac63
XiaolonY <xiaolony@hotmail.com>
2017-12-29 23:24:06 +0800

    add a new paper

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
 var htmlclean = require('gulp-htmlclean');
 
+var imagemin = require('gulp-imagemin');
+
 gulp.task('minify-css' , function () {
     return gulp.src('./public/**/*.css')
         .pipe(minifycss())
@@ -21,21 +23,26 @@ gulp.task('minify-html' , function () {
             removeScriptTypeAttributes: true,
             removeStyleLinkTypeAttributes: true,
             minifyJS: true,
-            minifyCSS: true
+            minifyCSS: true,
+            minifyURLs: true
         }))
-        .on('error', function (err) {
-            console.log('html Error!', err.message);
-            this.end();
-        })
         .pipe(gulp.dest('./public'));
 });
 
 gulp.task('minify-js', function () {
-    return gulp.src('./public/**/*.js')
+    return gulp.src('./public/**/**/*.js')
         .pipe(uglify())
         .pipe(gulp.dest('./public'));
 });
 
+gulp.task('images', function () {
+    gulp.src('./public/**/*.*')
+        .pipe(imagemin({
+            progressive: true
+        }))
+        .pipe(gulp.dest('./public/images'))
+});
+
 gulp.task('build', [
-    'minify-html', 'minify-css', 'minify-js'
+    'minify-html', 'minify-css', 'minify-js', 'images'
 ]);
