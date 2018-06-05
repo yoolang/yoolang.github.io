8dd64db879b271962ebcd79192e6fb1aaa8a9f51
Xiaolong Yang <xiaolony@hotmail.com>
2018-06-05 17:26:07 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,115 @@
-var gulp = require('gulp');
-var minifycss = require('gulp-minify-css');
-var uglify = require('gulp-uglify');
-var htmlmin = require('gulp-htmlmin');
-var htmlclean = require('gulp-htmlclean');
-var imagemin = require('gulp-imagemin');
+'use strict';
 
-// 压缩html
-gulp.task('minify-html', function() {
-    return gulp.src('./public/**/*.html')
-        .pipe(htmlclean())
-        .pipe(htmlmin({
-            removeComments: true,
-            minifyJS: true,
-            minifyCSS: true,
-            minifyURLs: true,
-        }))
-        .pipe(gulp.dest('./public/'))
+var gulp = require('gulp');
+var gulpIf = require('gulp-if');
+var gulpRev = require('gulp-rev');
+var gulpRevCollector = require('gulp-rev-collector');
+var gulpRevReplace = require('gulp-rev-replace');
+var gulpUglify = require('gulp-uglify');
+var gulpUniqueFiles = require('gulp-unique-files');
+var gulpUseRef = require('gulp-useref');
+var gulpCleanCSS = require('gulp-clean-css');
+var gulpResponsive = require('gulp-responsive');
+var gulpCheerio = require('gulp-cheerio');
+var del = require('del');
+var rename = require('rename');
+
+var dirs = {
+  public: 'public',
+  screenshots: 'public/build/screenshots'
+};
+
+gulp.task('useref', ['screenshot'], function() {
+  var assets = gulpUseRef.assets({
+    searchPath: 'public'
+  });
+
+  return gulp.src('public/**/*.html')
+    .pipe(assets)
+    .pipe(gulpUniqueFiles())
+    .pipe(gulpIf('*.css', gulpCleanCSS()))
+    .pipe(gulpIf('*.js', gulpUglify()))
+    .pipe(gulpRev())
+    .pipe(assets.restore())
+    .pipe(gulpUseRef())
+    .pipe(gulpRevReplace({
+      prefix: '/'
+    }))
+    .pipe(gulp.dest('public'));
 });
-// 压缩css
-gulp.task('minify-css', function() {
-    return gulp.src('./public/**/*.css')
-        .pipe(minifycss({
-            compatibility: 'ie8'
-        }))
-        .pipe(gulp.dest('./public/css'));
+
+gulp.task('screenshot:clean', function() {
+  return del([dirs.screenshots + '/**/*']);
 });
-// 压缩js
-gulp.task('minify-js', function() {
-    return gulp.src('./public/js/**/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('./public/js'));
+
+gulp.task('screenshot:rev', ['screenshot:clean'], function() {
+  return gulp.src('public/themes/screenshots/*.png')
+    .pipe(gulpRev())
+    .pipe(gulp.dest(dirs.screenshots))
+    .pipe(gulpRev.manifest())
+    .pipe(gulp.dest(dirs.screenshots));
 });
-// 压缩图片
-gulp.task('minify-images', function() {
-    return gulp.src('./public/images/**/*.*')
-        .pipe(imagemin(
-            [imagemin.gifsicle({'optimizationLevel': 3}),
-                imagemin.jpegtran({'progressive': true}),
-                imagemin.optipng({'optimizationLevel': 7}),
-                imagemin.svgo()],
-            {'verbose': true}))
-        .pipe(gulp.dest('./public/images'))
+
+gulp.task('screenshot:revreplace', ['screenshot:rev'], function() {
+  var destDir = '/build/screenshots';
+
+  return gulp.src([dirs.screenshots + '/rev-manifest.json', 'public/themes/index.html'])
+    .pipe(gulpRevCollector({
+      replaceReved: true,
+      dirReplacements: {
+        '/themes/screenshots': destDir
+      }
+    }))
+    .pipe(gulpCheerio(function($, file) {
+      $('img.plugin-screenshot-img.lazyload').each(function() {
+        var img = $(this);
+        var src = img.attr('data-src') || img.attr('data-org');
+        if (!src) return;
+
+        var jpgPath = replaceBackSlash(rename(src, {extname: '.jpg'}));
+        var jpg2xPath = replaceBackSlash(rename(jpgPath, {suffix: '@2x'}));
+        var srcset = [
+          jpgPath,
+          jpg2xPath + ' 2x'
+        ].join(', ');
+
+        img.attr('data-src', jpgPath)
+          .attr('data-srcset', srcset)
+          .attr('data-org', src);
+      });
+    }))
+    .pipe(gulp.dest('public/themes'));
 });
-// 默认任务
-gulp.task('default', [
-    'minify-html','minify-css','minify-js','minify-images'
-]);
+
+gulp.task('screenshot:resize', ['screenshot:rev'], function() {
+  return gulp.src(dirs.screenshots + '/*.png')
+    .pipe(gulpResponsive({
+      '*.png': [
+        {
+          width: '50%',
+          rename: {
+            extname: '.jpg'
+          }
+        },
+        {
+          rename: {
+            suffix: '@2x',
+            extname: '.jpg'
+          }
+        }
+      ]
+    }, {
+      progressive: true,
+      format: 'jpeg',
+      quality: 70,
+      stats: false
+    }))
+    .pipe(gulp.dest(dirs.screenshots));
+});
+
+gulp.task('screenshot', ['screenshot:rev', 'screenshot:resize', 'screenshot:revreplace']);
+gulp.task('default', ['useref', 'screenshot']);
+
+function replaceBackSlash(str) {
+  return str.replace(/\\/g, '/');
+}
